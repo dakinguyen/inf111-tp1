e9abf5a83427160fa82dd2a7ce17a0eef4d8e5e6
dakinguyen <44127008+dakinguyen@users.noreply.github.com>
2019-02-07 20:17:33 -0500

    comments

--- src/ProgrammePrincipale.java
+++ src/ProgrammePrincipale.java
@@ -6,25 +6,34 @@ public class ProgrammePrincipale {
 
     static public void main(String[] args){
 
+    	// Definir les variables qui servira de contenir les nombres et les expressions du jeu
         Nombre[] tab_num = new Nombre[TAILLE_NOMBRE];
         char[] expression = new char[TAILLE_CHAR];
         char rejouer = 'o';
         int[] tailleArray = new int[1];
 
+        // Tant que l'utilisateur veut rejouer on fait la boucle
         while (rejouer == 'o') {
+        	
+        	// Initialisation de du tableau de Nombres et du tableau d'expression
             functions.initExpression(expression);
             functions.genererNombres(tab_num);
+            functions.initBoolen(tab_num);
 
+            // Trouver le nombre d'operations qui se fera
             int nbOperations = UtilitaireFonctionsMath.alea(1,5);
 
+            // Trouver la cible
             int cible = functions.trouverCible(tab_num, nbOperations, tailleArray, expression);
 
+            // L'utilisateur effectue son tour
             functions.effectuerTour(tab_num, nbOperations, cible, expression, tailleArray);
 
+            // Demande si l'utilisateur veut rejouer
             rejouer = functions.veutRejouer();
 
         }
-
+        
         JOptionPane.showMessageDialog(null, "Merci d'avoir joue!");
     }
 }
